ab08b2351eed137c0517a69d277edd8effe21d0b
Alvaro <alvarojcd2004@gmail.com>
2021-01-26 21:04:47 -0500

    Fix Avatar

--- src/commands/General/avatar.js
+++ src/commands/General/avatar.js
@@ -1,20 +1,21 @@
 const BaseCommand = require('../../Utils/BaseCommand.js')
-
 module.exports = class AvatarCommand extends BaseCommand {
     constructor(client) {
-        super(client, {
-            name: 'avatar',
-            alias: ['av', 'pfp', 'foto']
-        })
+      super(client, {
+        name: "avatar",
+        alias: ["av", "pfp", "foto"],
+      });
     }
 
     async run(msg, args) {
         let user = msg.mentions.users.first() ||
             this.client.users.cache.get(args[0]) ||
-            this.client.users.cache.find(e => (e.username === args.join(" ") ||
-                (e.tag === args.join(" ")))) ||
-            (msg.guild ? (msg.guild.members.cache.find(e => (e.nickname === args.join(" ")))) : undefined) ||
-            (args[0] ? await this.client.users.fetch(args[0]).catch(() => { }) : undefined) || msg.author;
+            this.client.users.cache.find(e => (e.username == args.join(" ")) || (e.tag == args.join(" "))) ||
+            (msg.guild ? ((await msg.guild.members.fetch()).find(x => x.nickname === args.join(' '))) : undefined) ||
+            (args[0] ? await this.client.users.fetch(args[0]).catch(() => {}) : undefined) || msg.author;
+        if (user instanceof Discord.GuildMember) {
+            user = user.user;
+        }
         const embed = new Discord.MessageEmbed()
             .setColor(this.client.color)
             .setDescription(`Avatar de **${user.tag}**
