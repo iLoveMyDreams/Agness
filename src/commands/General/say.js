69d0382c82493ec97254d0ae9651f6de08b7c531
Alvaro <alvarojcd2004@gmail.com>
2021-01-27 11:06:51 -0500

    Say Command

--- src/commands/General/say.js
+++ src/commands/General/say.js
@@ -9,7 +9,7 @@ module.exports = class SayCommand extends BaseCommand {
     }
 
     async run(msg, args) {
-        if (msg.deletable) await message.delete();
+        if (msg.deletable) await msg.delete();
         if (!args[0]) return msg.channel.send('> A sos re trol ?')
         if (
             (msg.content.match(/@(everyone|here)/gi) ||
