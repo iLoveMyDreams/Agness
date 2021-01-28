c13116ccfc134d845887dfd7387f057e8a2b8c05
Drylotrans <35281350+Drylotrans@users.noreply.github.com>
2021-01-28 16:52:35 -0500

    .

--- src/commands/Config/rr.js
+++ src/commands/Config/rr.js
@@ -13,6 +13,8 @@ module.exports = class ReactionRoleCommand extends BaseCommand {
             category: 'Config'
         })
         this.types = ['normal', 'unique', 'only']
+        this.emojiUnicode = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
+        this.emojiDiscord = /<a?:[a-z]{3,32}:\d{16,19}>/gi
     }
 
     async run(msg, args) {
@@ -22,11 +24,28 @@ module.exports = class ReactionRoleCommand extends BaseCommand {
             embed.setDescription(`> Uso correcto: ${this.prefix}rrole [@role] [type] [messageID] <#channel>`)
             return msg.channel.send(embed)
         }
-        if(args[0].toLowerCase() === 'types'){
+        if (args[0].toLowerCase() === 'types') {
             embed.addField('Tipos:', `Normal => Se puede obtener y quitar el rol con la misma reacción.
 Unique => Solo se puede obtener, mas no quitar.
 Only => Solo se podrá obtener un reaction rol del mismo tipo en el mensaje.`)
-        return msg.channel.send(embed)
+            return msg.channel.send(embed)
+        } else if (args[0].toLowerCase() === 'delete') {
+            if (!args[1] || !args[2]) {
+                embed.setDescription(`> Uso correcto: ${this.prefix}rrole delete [emoji] [messageID]`)
+                return msg.channel.send(embed)
+            }
+            if (!this.emojiUnicode.test(args[1]) || !this.emojiDiscord.test(args[1])) {
+                embed.setDescription('> Debes poner un emoji.')
+                return msg.channel.send(embed)
+            }
+            let emojiID = this.emojiDiscord.test(args[1]) ? args[1].split(':')[1] : args[1]
+            let emojiCheck = await this.client.db.reaction.findOneAndDelete({ guildID: msg.guild.id, messageID: args[2], reaction: emojiID }).exec()
+            if (!emojiCheck) {
+                embed.setDescription('> No se pudo eliminar el reactionrol, comprueba si ya existe uno con esa ID de mensaje y emoji dentro del servidor.')
+                return msg.channel.send(embed)
+            }
+            embed.setDescription('> Reactionrol eliminado correctamente.')
+            return msg.channel.send(embed)
         }
         if (!args[1] || !args[2]) {
             embed.setDescription(`> Uso correcto: ${this.prefix}rrole [@role] [type] [messageID] <#channel>`)
@@ -48,9 +67,7 @@ Only => Solo se podrá obtener un reaction rol del mismo tipo en el mensaje.`)
 
         //type: 1
 
-        if (
-            !this.types.includes(args[1].toLowerCase())
-        ) {
+        if (!this.types.includes(args[1].toLowerCase())) {
             embed.setDescription('> No es un tipo valido de reaction rol.')
                 .addField('Tipos:', `Normal => Se puede obtener y quitar el rol con la misma reacción.
 Unique => Solo se puede obtener, mas no quitar.
