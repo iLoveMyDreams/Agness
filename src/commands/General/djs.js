ab63667b4b2ebd5ce976ca9ce43420a49ac23b76
Alvaro <alvarojcd2004@gmail.com>
2021-01-28 10:48:42 -0500

    DJS Command

---
+++ src/commands/General/djs.js
@@ -0,0 +1,18 @@
+const BaseCommand = require('../../Utils/BaseCommand.js')
+const fetch = require('node-fetch');
+module.exports = class DJSCommand extends BaseCommand {
+    constructor(client) {
+        super(client, {
+            name: 'djs'
+        })
+    }
+
+    async run(msg, args) {
+        if (!args[0]) return msg.channel.send('¿Qué quieres que busque?');
+        let response = await fetch(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(args.join(' '))}`).catch(() => { });
+        if (!response) return msg.channel.send('No obtuve resultados');
+        let embed = await response.json().catch(() => { });
+        if (!embed) return msg.channel.send("No encontre nada en la documentación.")
+        message.channel.send({ embed: embed }).catch(() => { });
+    }
+}
