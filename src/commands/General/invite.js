07dc1ddb52d6854e78264d70a0780b98b727e48d
Alvaro <alvarojcd2004@gmail.com>
2021-02-03 16:32:53 -0500

    .

---
+++ src/commands/General/invite.js
@@ -0,0 +1,21 @@
+const BaseCommand = require('../../Utils/BaseCommand.js')
+
+module.exports = class InviteCommand extends BaseCommand {
+    constructor(client) {
+        super(client, {
+            name: 'invite'
+        })
+    }
+
+    async run(msg, args) {
+        const embed = new Discord.MessageEmbed()
+        .setColor(this.client.color)
+        .setDescription(`Thank you for inviting me to your server! You will not regret.
+> [This is my invite link.](https://discord.com/api/oauth2/authorize?client_id=798573830645874718&permissions=8&scope=bot)
+In case you have any doubts, here is the invitation link from my support server.
+> [Support Server](https://discord.gg/K63NqEDm86)`)
+        .setAuthor('Tkm <3')
+        .setTimestamp()
+        msg.channel.send(embed)
+    }
+}
