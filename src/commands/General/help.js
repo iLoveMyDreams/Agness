079ee5cac15a46245c6afd24a8184fe9648d5667
Alvaro <alvarojcd2004@gmail.com>
2021-02-07 15:08:55 -0500

    .

--- src/commands/General/help.js
+++ src/commands/General/help.js
@@ -20,8 +20,8 @@ If you need more detailed information about each command, you can use:
             .addField('Categories', `> ${this.prefix}help Config • Configuration Commands
 > ${this.prefix}help General • Commands in general
 > ${this.prefix}help Images • Images of all types`)
-            .addField('Links', '**[Bot Invite](https://discord.com/api/oauth2/authorize?client_id=798573830645874718&permissions=8&scope=bot) | [Support Server](https://discord.gg/K63NqEDm86) | [Top.gg](https://top.gg/bot/798573830645874718)**'))
-						.setFooter('<> Optional | [] Required')
+            .addField('Links', '**[Bot Invite](https://discord.com/api/oauth2/authorize?client_id=798573830645874718&permissions=8&scope=bot) | [Support Server](https://discord.gg/K63NqEDm86) | [Top.gg](https://top.gg/bot/798573830645874718)**')
+            .setFooter('<> Optional | [] Required'))
         const category = this.client.commands.filter((x) => x.category.toLowerCase() == args[0].toLowerCase()).array();
         const cmd = this.client.commands.get(args[0].toLowerCase()) || this.client.commands.find((x) => x.alias && x.aliases.includes(args[0].toLowerCase()));
 
