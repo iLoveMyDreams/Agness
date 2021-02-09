06f8ce3ced658f150938f04663f6d98fddb2727c
Alvaro <alvarojcd2004@gmail.com>
2021-02-09 17:33:01 -0500

    .

--- src/commands/General/comment.js
+++ src/commands/General/comment.js
@@ -16,32 +16,50 @@ module.exports = class CommentCommand extends BaseCommand {
         const embed = new Discord.MessageEmbed()
             .setDescription(`You must put a valid property.
 > \`${this.prefix}comment suggest [new suggest]\`
-> \`${this.prefix}comment bug [new bug]\``)
+> \`${this.prefix}comment bug [command/event] [description]\``)
             .setColor(this.client.color)
             .setFooter(`Remember that you can also insert images or gifs.`)
         if (!args[0]) return msg.channel.send(embed)
         switch (args[0]) {
             case 'suggest': {
-                if (!args[1]) return msg.channel.send(`Tell me your suggestion.`)
-                if (args.slice(1).join(' ').length > 500) return msg.channel.send(`The suggestion must not exceed 500 characters`)
-                if(msg.attachments.first() && this.types.some(x => msg.attachments.first().url.toLowerCase().endsWith(x))){
-                    var name = 'suggest.png'
-                    if(msg.attachments.first().url.toLowerCase().endsWith('.gif')) name = 'suggest.gif'
-                    const att = new Discord.MessageAttachment(msg.attachments.first().url, name)
-                }
                 const embedSuggest = new Discord.MessageEmbed()
-                 .setAuthor(`New Suggest`)
-                 .setDescription(`**Author:** ${msg.user.tag} | ${msg.user.id}
+                if (!args[1]) return msg.channel.send(`Tell me your suggestion.
+> \`${this.prefix}comment suggest [new suggest]\``)
+                if (args.slice(1).join(' ').length > 500) return msg.channel.send(`The suggestion must not exceed 500 characters`)
+                if (msg.attachments.first() && this.types.some(x => msg.attachments.first().url.toLowerCase().endsWith(x))) {
+                    const name = 'suggest.png'
+                    if (msg.attachments.first().url.toLowerCase().endsWith('.gif')) name = 'suggest.gif'
+                    const att = new Discord.MessageAttachment(msg.attachments.first().url, name)
+                    embedSuggest.attachFiles([att])
+                        .setImage(`attachment://${name}`);
+                }
+                embedSuggest.setAuthor(`New Suggest`)
+                    .setDescription(`**Author:** ${msg.user.tag} | ${msg.user.id}
 **Guild:** ${msg.guild.name} | ${msg.guild.id}
 **Message:** ${args.slice(1).join(' ')}`)
-                if(att){
-                    embedSuggest.attachFiles([att])
-                    .setImage(`attachment://${name}`);
-                }
+                    .setTimestamp()
                 canalSuggest.send(embedSuggest)
                 break;
             }
             case 'bug': {
+                const embedReport = new Discord.MessageEmbed()
+                if (!args[1]) return msg.channel.send(`Tell me the bug to report.
+> \`${this.prefix}comment bug [command/event] [description]\``)
+                if (args.slice(2).join(' ').length > 500) return msg.channel.send(`The bug report must not exceed 500 characters.`)
+                if (msg.attachments.first() && this.types.some(x => msg.attachments.first().url.toLowerCase().endsWith(x))) {
+                    const name = 'suggest.png'
+                    if (msg.attachments.first().url.toLowerCase().endsWith('.gif')) name = 'suggest.gif'
+                    const att = new Discord.MessageAttachment(msg.attachments.first().url, name)
+                    embedReport.attachFiles([att])
+                        .setImage(`attachment://${name}`);
+                }
+                embedReport.setAuthor(`New Bug Report`)
+                    .setDescription(`**Author:** ${msg.user.tag} | ${msg.user.id}
+**Guild:** ${msg.guild.name} | ${msg.guild.id}
+**Command or event:** ${args[1]}
+**Message:** ${args.slice(2).join(' ')}`)
+                    .setTimestamp()
+                canalReport.send(embedReport)
                 break;
             }
             default:
