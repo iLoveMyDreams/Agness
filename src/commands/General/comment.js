71de091b941f0cb64eedab91b795d8771776c1aa
Alvaro <alvarojcd2004@gmail.com>
2021-02-09 13:06:28 -0500

    .

---
+++ src/commands/General/comment.js
@@ -0,0 +1,51 @@
+const { DiscordAPIError } = require('discord.js');
+const BaseCommand = require('../../Utils/BaseCommand.js');
+
+module.exports = class CommentCommand extends BaseCommand {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            guildOnly: false
+        });
+        this.types = ['.png', '.jpg', '.jpeg', '.gif'];
+    }
+
+    async run(msg, args) {
+        const canalSuggest = await this.client.channels.fetch(process.env.SUGGEST_CHANNEL);
+        const canalReport = await this.client.channels.fetch(process.env.REPORT_CHANNEL);
+        const embed = new Discord.MessageEmbed()
+            .setDescription(`You must put a valid property.
+> \`${this.prefix}comment suggest [new suggest]\`
+> \`${this.prefix}comment bug [new bug]\``)
+            .setColor(this.client.color)
+            .setFooter(`Remember that you can also insert images or gifs.`)
+        if (!args[0]) return msg.channel.send(embed)
+        switch (args[0]) {
+            case 'suggest': {
+                if (!args[1]) return msg.channel.send(`Tell me your suggestion.`)
+                if (args.slice(1).join(' ').length > 500) return msg.channel.send(`The suggestion must not exceed 500 characters`)
+                if(msg.attachments.first() && this.types.some(x => msg.attachments.first().url.toLowerCase().endsWith(x))){
+                    var name = 'suggest.png'
+                    if(msg.attachments.first().url.toLowerCase().endsWith('.gif')) name = 'suggest.gif'
+                    const att = new Discord.MessageAttachment(msg.attachments.first().url, name)
+                }
+                const embedSuggest = new Discord.MessageEmbed()
+                 .setAuthor(`New Suggest`)
+                 .setDescription(`**Author:** ${msg.user.tag} | ${msg.user.id}
+**Guild:** ${msg.guild.name} | ${msg.guild.id}
+**Message:** ${args.slice(1).join(' ')}`)
+                if(att){
+                    embedSuggest.attachFiles([att])
+                    .setImage(`attachment://${name}`);
+                }
+                canalSuggest.send(embedSuggest)
+                break;
+            }
+            case 'bug': {
+                break;
+            }
+            default:
+                break;
+        }
+    }
+};
