c809f467e18322d821afa4870b538664c3652bd6
Alvaro <alvarojcd2004@gmail.com>
2021-02-13 18:01:33 -0500

    .

--- src/commands/General/guilds.js
+++ src/commands/General/guilds.js
@@ -1,8 +1,3 @@
-/*
-    Copied from https://github.com/AndreMor955/gidget
-    Thanks Andremor ❤️
-*/
-
 const BaseCommand = require('../../Utils/BaseCommand.js');
 
 module.exports = class GuildsCommand extends BaseCommand {
@@ -15,11 +10,20 @@ module.exports = class GuildsCommand extends BaseCommand {
     }
 
     async run(msg) {
-        const servers = (await this.client.shard.fetchClientValues('guilds.cache.size')).reduce((acc, guildCount) => acc + guildCount, 0);
-        const users = (await this.client.shard.fetchClientValues('users.cache.size')).reduce((acc, userCount) => acc + userCount, 0);
+
+        const promises = [
+            this.client.shard.fetchClientValues('guilds.cache.size'),
+            this.client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
+        ];
+        
+        Promise.all(promises)
+            .then(results => {
+                const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+                const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
         msg.channel.send(new Discord.MessageEmbed()
-            .setDescription(`At this moment, I'm in **${servers}** servers and with **${users}** cached users.`)
+            .setDescription(`At this moment, I'm in **${totalGuilds}** servers and with **${totalMembers}** cached users.`)
             .setFooter(`This server is on the shard ${this.client.shard.ids.map((i) => `${i}`).join(', ')}`)
             .setColor(this.client.color));
+            })
     }
 };
