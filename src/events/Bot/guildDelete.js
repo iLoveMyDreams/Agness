3afd0bfef2388a3ec9c9729a12ec18a7987c4266
Alvaro <alvarojcd2004@gmail.com>
2021-02-18 17:21:45 -0500

    .

--- src/events/Bot/guildDelete.js
+++ src/events/Bot/guildDelete.js
@@ -16,11 +16,11 @@ module.exports = class GuildDeleteEvent {
 
         const guilds = (await this.client.shard.fetchClientValues('guilds.cache.size')).reduce((acc, guildCount) => acc + guildCount, 0);
         const canal = await this.client.channels.fetch(process.env.SERVERS_CHANNEL);
-        const owner = await this.client.users.fetch(guild.ownerID).catch(() => null) || guild.owner.user
+        const owner = await this.client.users.fetch(guild.ownerID).catch(() => null)
         if (canal)
             canal.send(new Discord.MessageEmbed()
                 .setAuthor('Server Remove', this.client.user.displayAvatarURL({ dynamic: true }))
-                .setDescription(`**Name**: ${guild.name}\n**Members**: ${guild.memberCount}\n**Owner**: ${owner.tag} | ${owner.id}`)
+                .setDescription(`**Name**: ${guild.name}\n**Members**: ${guild.memberCount}\n${owner ? `**Owner**: ${owner.tag} | ${owner.id}`: ''}`)
                 .setThumbnail(guild.iconURL({ dynamic: true }))
                 .setColor('RED')
                 .setFooter(`Servers count: ${guilds}`)
