a77188198c163d08c608cf66854eef6e4c6c6088
Alvaro <alvarojcd2004@gmail.com>
2021-02-18 16:35:08 -0500

    Posibles errores en el evento guildDelete

--- src/events/Bot/guildDelete.js
+++ src/events/Bot/guildDelete.js
@@ -5,12 +5,13 @@ module.exports = class GuildDeleteEvent {
     }
 
     async run(guild) {
-        await this.client.db.prefix.findOneAndDelete({ _id: guild.id })
-        await this.client.db.welcome.findOneAndDelete({ guildID: guild.id })
-        await this.client.db.leave.findOneAndDelete({ guildID: guild.id })
-        await this.client.db.reaction.deleteMany({ guildID: guild.id })
-        await this.client.db.embed.deleteMany({ guildID: guild.id })
-        await this.client.db.tags.deleteMany({ guildID: guild.id })
+        try{
+        await this.client.db.prefix.findOneAndDelete({ _id: guild.id }).catch(() => { })
+        await this.client.db.welcome.findOneAndDelete({ guildID: guild.id }).catch(() => { })
+        await this.client.db.leave.findOneAndDelete({ guildID: guild.id }).catch(() => { })
+        await this.client.db.reaction.deleteMany({ guildID: guild.id }).catch(() => { })
+        await this.client.db.embed.deleteMany({ guildID: guild.id }).catch(() => { })
+        await this.client.db.tags.deleteMany({ guildID: guild.id }).catch(() => { })
         
         const guilds = (await this.client.shard.fetchClientValues('guilds.cache.size')).reduce((acc, guildCount) => acc + guildCount, 0);
         const canal = await this.client.channels.fetch(process.env.SERVERS_CHANNEL);
@@ -23,5 +24,8 @@ module.exports = class GuildDeleteEvent {
                 .setColor('RED')
                 .setFooter(`Servers count: ${guilds}`)
                 .setTimestamp());
+        }catch (e){
+            console.log(e.message || e)
+        }
     }
 };
