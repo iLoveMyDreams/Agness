ec76872bd49cc12f64ccd19ae8ed985c6afe9919
Alvaro <alvarojcd2004@gmail.com>
2021-02-18 10:58:36 -0500

    Eliminación de datos

--- src/events/Bot/guildDelete.js
+++ src/events/Bot/guildDelete.js
@@ -5,6 +5,13 @@ module.exports = class GuildDeleteEvent {
     }
 
     async run(guild) {
+        await this.client.db.prefix.findOneAndDelete({ _id: guild.id })
+        await this.client.db.welcome.findOneAndDelete({ guildID: guild.id })
+        await this.client.db.leave.findOneAndDelete({ guildID: guild.id })
+        await this.client.db.reactions.deleteMany({ guildID: guild.id })
+        await this.client.db.embed.deleteMany({ guildID: guild.id })
+        await this.client.db.tags.deleteMany({ guildID: guild.id })
+        
         const guilds = (await this.client.shard.fetchClientValues('guilds.cache.size')).reduce((acc, guildCount) => acc + guildCount, 0);
         const canal = await this.client.channels.fetch(process.env.SERVERS_CHANNEL);
         const owner = await this.client.users.fetch(guild.ownerID);
